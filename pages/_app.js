5d6dbe56070ffdb6caae86e1975144e351f7e1e2
Mitesh2020 <miteshofficial007@gmail.com>
2024-08-16 11:45:50 +0530

    Initial Commmit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { Poppins } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <main className={`${inter.className}`}>
+        <Header />
+        <div className="min-h-screen max-w-screen-2xl mx-auto">
+          <Component {...pageProps} />
+          <Toaster position="top-center" />
+        </div>
+        <Footer />
+      </main>
+    </SessionProvider>
+  );
 }
